3e5b7bb8c801f32bc03aaaab72c395ac2ba601f4
Dewald Fortier <DewaldF@UIPDEWFORT1.mgsops.net>
2016-05-23 16:49:52 +0100

    Added react Started implementation for filter on the dashboard controller

---
+++ Derivco.Orniscient/Derivco.Orniscient.Viewer/Scripts/Dashboard/Dashboard.jsx
@@ -0,0 +1,28 @@
+﻿var Dashboard = React.createClass({
+    render: function() {
+        return (
+            <div className="container bigContainer ">
+                <div className="row">
+                    <div className="col-md-3 padding20">
+                        <div className="row">
+                            <h3>Filter options</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="col-md-9" id="mynetwork">
+
+                    </div>
+                </div>
+            </div>
+        );
+    },
+    ComponentDidMount: function () {
+        console.log('componentDidUpdate called');
+        orniscient.init();
+    }
+});
+
+ReactDOM.render(
+        <Dashboard />,
+        document.getElementById('dashboard')
+    )
--- Derivco.Orniscient/Derivco.Orniscient.Viewer/Scripts/orniscient.js
+++ Derivco.Orniscient/Derivco.Orniscient.Viewer/Scripts/orniscient.js
@@ -1,4 +1,5 @@
-﻿$(function () {
+﻿
+(function (orniscient, $, undefined) {
 
     var hub = $.connection.orniscientHub,
         nodes = new vis.DataSet([]),
@@ -35,26 +36,40 @@
         edges: edges
     };
 
-    var network = new vis.Network(container, data, options);
-    network.on("selectNode", function (params) {
-        if (params.nodes.length == 1) {
-            if (network.isCluster(params.nodes[0]) == true) {
-                network.openCluster(params.nodes[0]);
+    orniscient.init = function () {
+
+        var network = new vis.Network(container, data, options);
+        network.on("selectNode", function (params) {
+            if (params.nodes.length == 1) {
+                if (network.isCluster(params.nodes[0]) == true) {
+                    network.openCluster(params.nodes[0]);
+                }
             }
-        }
-    });
-    $.connection.hub.start()
-		.then(init);
+        });
+
+        console.log('doing the init now');
+        $.connection.hub.start().then(init);
+
+        //add client side methods for updates
+        $.extend(hub.client, {
+            grainActivationChanged: function (diffModel) {
+                $.each(diffModel.NewGrains, function (index, grainData) {
+                    addToNodes(grainData);
+                });
+            }
+        });
+    }
 
     function init() {
+        console.log('doing init function now');
         return hub.server.getCurrentSnapshot()
             .done(function (data) {
-                
-                $.each(data, function(index, grainData) {
+                console.log('return from server with grain count of : ' + data.length);
+                $.each(data, function (index, grainData) {
                     addToNodes(grainData);
                 });
             })
-            .fail(function(data) {
+            .fail(function (data) {
                 alert('Oops, we cannot connect to the server...');
             });
     }
@@ -78,13 +93,99 @@
         }
     }
 
-    //add client side methods for updates
-    $.extend(hub.client, {
-        grainActivationChanged: function (diffModel) {
-            $.each(diffModel.NewGrains, function (index, grainData) {
-                addToNodes(grainData);
-            });
-        }
-    });
 
-});
+
+}(window.orniscient = window.orniscient || {}, jQuery));
+
+
+
+//$(function () {
+
+//    var hub = $.connection.orniscientHub,
+//        nodes = new vis.DataSet([]),
+//        edges = new vis.DataSet([]),
+//        container = document.getElementById('mynetwork'),
+//        arrows = { to: { scaleFactor: 1 } };
+
+//    var options = {
+//        autoResize: true,
+//        height: '100%',
+//        nodes: {
+//            shape: 'dot',
+//            scaling: {
+//                min: 10,
+//                max: 30,
+//                label: {
+//                    min: 8,
+//                    max: 30,
+//                    drawThreshold: 12,
+//                    maxVisible: 20
+//                }
+//            }
+//        },
+//        interaction: {
+//            hover: true,
+//            navigationButtons: true
+//        },
+//        layout: {
+//            randomSeed: 2
+//        }
+//    };
+//    var data = {
+//        nodes: nodes,
+//        edges: edges
+//    };
+
+//    var network = new vis.Network(container, data, options);
+//    network.on("selectNode", function (params) {
+//        if (params.nodes.length == 1) {
+//            if (network.isCluster(params.nodes[0]) == true) {
+//                network.openCluster(params.nodes[0]);
+//            }
+//        }
+//    });
+//    $.connection.hub.start()
+//		.then(init);
+
+//    function init() {
+//        return hub.server.getCurrentSnapshot()
+//            .done(function (data) {
+
+//                $.each(data, function (index, grainData) {
+//                    addToNodes(grainData);
+//                });
+//            })
+//            .fail(function (data) {
+//                alert('Oops, we cannot connect to the server...');
+//            });
+//    }
+
+//    function addToNodes(grainData) {
+
+//        //add the node
+//        nodes.add({
+//            id: grainData.Id,
+//            label: grainData.GrainName,
+//            color: grainData.Colour,
+//            silo: 'C'
+//        });
+
+//        //add the edge (link)
+//        if (grainData.LinkToId !== '') {
+//            edges.add({
+//                from: grainData.Id,
+//                to: grainData.LinkToId
+//            });
+//        }
+//    }
+
+//    //add client side methods for updates
+//    $.extend(hub.client, {
+//        grainActivationChanged: function (diffModel) {
+//            $.each(diffModel.NewGrains, function (index, grainData) {
+//                addToNodes(grainData);
+//            });
+//        }
+//    });
+
+//});
