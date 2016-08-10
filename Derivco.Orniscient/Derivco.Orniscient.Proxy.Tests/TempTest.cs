2ecca0f634bcf9ebe074bae95fce3cb39b481fbd
Dewald Fortier <Dewald.Fortier@derivco.co.uk>
2016-08-10 09:32:41 +0100

    Added Tests project for proxy Installed XUnit Added TempTest to ensure that the tests will run on TeamCity.

---
+++ Derivco.Orniscient/Derivco.Orniscient.Proxy.Tests/TempTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Derivco.Orniscient.Proxy.Tests
+{
+    public class TempTest
+    {
+        [Fact]
+        public void CheckXUnitWorking()
+        {
+            Assert.True(1==1);
+        }
+    }
+}
