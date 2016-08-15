88a8b1c23ff592c6555f7dcf62a8c69f94d1bf73
Shantal Naidoo <Shantal.Naidoo@Derivco.co.uk>
2016-08-15 17:26:53 +0100

    Added Unit Tests for TypeMethodGrain

---
+++ Derivco.Orniscient/Derivco.Orniscient.Proxy.Tests/Grains/TypeMethodGrainTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Derivco.Orniscient.Proxy.Grains;
+using Orleans.Runtime;
+using Orleans.TestingHost;
+using Xunit;
+
+namespace Derivco.Orniscient.Proxy.Tests.Grains
+{
+    public class TypeMethodGrainTests : TestCluster
+    {
+        public TypeMethodGrainTests()
+        {
+            Deploy();
+        }
+
+        [Fact]
+        public async Task GetAvailableMethods_FooGrain_ShouldReturnSixMethods()
+        {
+            const int expected = 6;
+            var methodGrain = GrainFactory.GetGrain<ITypeMethodsGrain>("TestGrains.Grains.FooGrain");
+
+            var reply = await methodGrain.GetAvailableMethods();
+            
+            Assert.NotNull(reply);
+            Assert.Equal(expected, reply.Count);
+        }
+
+        [Fact]
+        public async Task GetAvailableMethods_FirstGrain_ShouldReturnZeroMethods()
+        {
+            const int expected = 0;
+            var methodGrain = GrainFactory.GetGrain<ITypeMethodsGrain>("TestGrains.Grains.FirstGrain");
+
+            var reply = await methodGrain.GetAvailableMethods();
+            
+            Assert.NotNull(reply);
+            Assert.Equal(expected, reply.Count);
+        }
+
+        [Fact]
+        public async Task GetAvailableMethods_ForAnInvalidGrain_ShouldThrowOrleansException()
+        {
+            var methodGrain = GrainFactory.GetGrain<ITypeMethodsGrain>("Test");
+
+            await Assert.ThrowsAsync<OrleansException>(async () => await methodGrain.GetAvailableMethods());
+        }
+    }
+}
